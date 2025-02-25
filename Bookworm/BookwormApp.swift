7311c662ba7e78127a1287f6de4dd6d25b6e30db
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-25 09:25:00 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Shashank B on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Shashank B on 25/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
